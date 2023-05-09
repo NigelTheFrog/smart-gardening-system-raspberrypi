67b8907dbdd5eee07d388f84ab4d1d55ded64cb6
nigelwilliam16 <s160419017@student.ubaya.ac.id>
2023-05-09 22:40:20 +0700

    rsapberry back up & finalisation web

--- raspberryapp.py
+++ raspberryapp.py
@@ -1,7 +1,7 @@
+#main app.py
 import json
 import requests
 import time
-import RPi.GPIO as gp  
 from seeed_dht import DHT
 from grove.adc import ADC
 from grove.grove_light_sensor_v1_2 import GroveLightSensor
@@ -50,14 +50,12 @@ def handle_method_request(request):
                 for d in servo_degree:
                     if d[0] == i['id']:   
                         if d[1] == 0: 
-                            print("Servo berputar menuju 180")                                                
                             for s in range(0, 180, +1):
                                 servo.setAngle(s)
                                 time.sleep(0.003)
                             d[1] = 1  
                         else:
                             servo.setAngle(180)
-                            print("Servo stay di 180")              
                 insert_log(i['id'],str(request.payload), "Papan terbuka")  
             elif i['nama_sensor'] == 'Sensor Suhu':
                 GroveRelay(int(i['port_aktuator'][1:])).on()
@@ -79,14 +77,12 @@ def handle_method_request(request):
                 for d in servo_degree:
                     if d[0] == i['id']:
                         if d[1] == 1:  
-                            print("Servo berputar menuju 0")                               
                             for s in range(180, 0, -1):
                                 servo.setAngle(s)
                                 time.sleep(0.003)
                             d[1] = 0
                         else:
                             servo.setAngle(0)
-                            print("Servo stay di 0")
                 insert_log(i['id'],str(request.payload), "Papan tertutup")  
             elif i['nama_sensor'] == 'Sensor Suhu':
                 GroveRelay(int(i['port_aktuator'][1:])).off()
@@ -153,3 +149,117 @@ while True:
     time.sleep(5)       
 # except:
 #     print("Connection failure")          
+
+#phtest.py
+
+
+# import board
+# import busio
+# import time
+# import sys
+# import adafruit_ads1x15.ads1115 as ADS
+# from adafruit_ads1x15.analog_in import AnalogIn
+
+# # Setup 
+# i2c = busio.I2C(board.SCL, board.SDA)
+# ads = ADS.ADS1115(i2c)
+
+# while True:
+#     buf = list()
+    
+#     for i in range(10): # Take 10 samples
+#         buf.append(AnalogIn(ads, ADS.P1).voltage)
+#     buf.sort() # Sort samples and discard highest and lowest
+#     buf = buf[2:-2]
+#     avg = (sum(map(float,buf))/6) # Get average value from remaining 6
+
+#     print(round(avg,2),'V')
+#     time.sleep(2)
+
+# import time
+# from grovepi import *
+
+# # Connect the PH-4502C pH sensor to analog port A4
+# sensor = 4
+
+
+# # Read the analog voltage output from the sensor
+# def read_voltage():
+#     voltage = analogRead(sensor)
+#     if voltage == -1:
+#         return None
+#     else:
+#         return voltage / 1023.0 * 5.0
+
+# # Continuously read and print the voltage output
+# while True:
+#     voltage = read_voltage()
+#     if voltage is not None:
+#         ph = voltage_to_ph(voltage)
+#         print("Voltage: %.2f V, pH: %.2f" % (voltage, ph))
+#     time.sleep(1)
+#     # except KeyboardInterrupt:
+#     #     print("Exiting...")
+#     #     break
+#     # except IOError:
+#     #     print("Error: Unable to read analog value")
+
+import time
+from grove.i2c import Bus
+from grove.adc import ADC
+
+# Connect the PH-4502C pH sensor to the Grove Base Hat ADC channel A0
+adc = ADC(busnum=1, address=0x48)
+channel = 4
+
+# Calibration parameters for the pH sensor
+pH_7 = 1.81  # Voltage output of the sensor at pH 7
+slope = -0.247  # Slope of the linear equation relating pH to voltage
+
+# Read the analog voltage output from the sensor and convert it into a pH value
+def read_ph():
+    voltage = adc.read_voltage(channel)
+    pH = slope * (voltage - pH_7) + 7.0
+    return pH
+
+# Continuously read and print the pH value
+while True:
+    pH = read_ph()
+    print("pH value: ", pH)
+    time.sleep(1)
+    
+# import time
+# import grovepi
+
+# # Setup ADC
+# grovepi.pinMode(4,"INPUT")
+
+# def read_voltage():
+#     # Read voltage from ADC
+#     sensor = 4
+#     time.sleep(0.1) # add delay
+#     sensor_value = grovepi.analogRead(sensor)
+#     # Convert ADC value to voltage
+#     voltage = (float)(sensor_value * 5) / 1024
+#     return voltage
+
+# def read_ph():
+#     # Read pH value from sensor
+#     voltage = read_voltage()
+#     ph = 3.5 * voltage - 1.75
+#     return ph
+
+# if __name__ == '__main__':
+#     print('\n\n\n')
+#     print('---- Raspberry Pi Grove Base Hat pH4502C Calibration ----')
+#     input('Press Enter once you have connected the sensor to A4...')
+#     print('Starting readings. Press CTRL+C to stop...')
+#     try:
+#         while True:
+#             volt = read_voltage()
+#             ph = read_ph()
+#             print('Voltase: ', volt)
+#             print('pH:', round(ph, 2))
+#             time.sleep(2)
+#     except KeyboardInterrupt:
+#         pass
